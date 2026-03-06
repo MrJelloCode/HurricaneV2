001ec1e0382ca9ceffd88c8ef0a88c2598d72704
Malaravan Vijayakumar <MalaravanV5@gmail.com>
2026-03-06 16:44:27 -0500

    Created TurretSubsystem.java with needed constants.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/subsystem/TurretSubsystem.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.pedroPathing.subsystem;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+public class TurretSubsystem {
+
+    private final DcMotorEx turretMotor;
+
+    /* ================= ENCODER CONSTANTS ================= */
+
+    private static final double TICKS_PER_REV = 435;   // motor encoder CPR
+    private static final double GEAR_RATIO = 3;      // planetary ratio
+
+    /* ================= SAFETY LIMITS ================= */
+
+    private static final double MAX_POWER = 1;
+    private static final double MIN_ANGLE = -180.0;
+    private static final double MAX_ANGLE = 180.0;
+
+    /* ================= CONSTRUCTOR ================= */
+
+    public TurretSubsystem(DcMotorEx turretMotor) {
+        this.turretMotor = turretMotor;
+    }
+
+}
